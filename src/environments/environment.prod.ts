55e22c8cd552f71cff47d831b34661fefdea5966
fajarbass <sebastian.fajr@gmail.com>
2022-11-07 07:34:24 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyDOCGeNGWMA3xXhNWewRK_8QlY68mDhHdk",
-    authDomain: "fajar-sebastian.firebaseapp.com",
-    databaseURL: "https://emplo-yes.firebaseio.com/",
-    projectId: "fajar-sebastian",
-    storageBucket: "fajar-sebastian.appspot.com",
-    messagingSenderId: "76648213473",
-    appId: "1:76648213473:web:a4598d89cc629a29ca00a0",
-    measurementId: "G-GCNTHFJJ8S"
+    apiKey: "apikey",
+    authDomain: "authdomain",
+    databaseURL: "databaseurl",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   }
 };
